8a3785557f63fe8ea0b00231371b90b9cced34af
$CIRCLE_PROJECT_USERNAME <eng@rightbalance.io>
2023-06-20 16:51:58 +0000

    Updated via CI Pipeline

---
+++ _next/static/hGWrPDmLNyBG7iMyZ8qDJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-00d4b4798251442e.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
